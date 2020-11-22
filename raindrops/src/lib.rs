f208b0bbf9d1a3bba89ec827b7914ac5ad4b0413
13dev <qwerty124563@gmail.com>
2020-11-22 17:08:04 +0000

    add solution to raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut result = String::new();
+
+    if n % 3 == 0 {
+        result.push_str("Pling");
+    }
+
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    }
+
+    if n % 7 == 0 {
+        result.push_str("Plong")
+    }
+
+    if result.is_empty() {
+        return n.to_string();
+    }
+
+    result
 }
